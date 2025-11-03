46533f60f195e65dbc9db4f1b7df0bd74830f7c5
Daniel Clovis da Costa <danielclovis233@gmail.com>
2025-11-03 17:27:51 -0300

    Add files via upload

--- exercicios7/ex006/script.js
+++ exercicios7/ex006/script.js
@@ -1,3 +1,8 @@
 let nome = prompt("Digite o primeiro Nome")
 
-alert((nome.toUpperCase() {nome.length}))
+alert(nome.toUpperCase());
+alert(nome.length);
+
+// nome.length serve para conta a quantidade de caracteres que tem na variavel nome.
+
+// o nome.toLocaleUpperCase() serve para deixar todas as letras maiusculas.
